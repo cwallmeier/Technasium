b672945f1adbd0a2519d1837a1f14f49cc81da95
carowall <c.wallmeier@tudelft.nl>
2026-09-14 12:04:05 +0200

    small fixes to the doublet class

--- LittleGeothermalModel/doublet.py
+++ LittleGeothermalModel/doublet.py
@@ -14,7 +14,7 @@ class Doublet:
 
     def __init__(self,
                  # Input parameters that are likely to change:
-                 # The numbers you see here are default values. Don't change them. If you want to change something,
+                 # The numbers you see here are default values. Don't change them here. If you want to change something,
                  # then define it in the main file (Notebook_2026) when calling Doublet.
 
                  # Geometry
@@ -27,14 +27,14 @@ class Doublet:
                  Temp_grad=30,  # How fast the temperature increases with depth (°C/km)
 
                  # Engineering
-                 flow_m3_h=200,  # volume flow rate m3/h
+                 flow_m3_h=200,  # water flow rate m3/h
                  w_space=1200,  # well spacing in meters
-                 T_inj=30,  # injection temperature degC
+                 T_inj=30,  # injection temperature °C
 
                  # Efficiency
                  pump_effi=60,  # efficiency of the pump, in percent
 
-                 # Input parameters that usually don't change so much. But feel free to vary them!
+                 # Input parameters that usually don't change so much. But you can vary them if you have reasons to do so.
                  T_surf=10,  # Average temperature on the surface of the earth (°C)
                  rho_rock=2300,  # rock density kg/m3
                  Cp_rock=1,  # specific heat capacity of the rock kJ/(kgK)
@@ -43,6 +43,8 @@ class Doublet:
                  ):
 
         # Don't touch this block.
+        # What happens here: The variables that are defined above are assigned to the doublet. This is a typical structure of
+        # code like this.
         # Input parameters Geometry
         self.r_d = depth_m
         self.r_h = thickn_m
@@ -51,14 +53,14 @@ class Doublet:
         self.rho_rock = rho_rock
         self.Cp_rock = Cp_rock
         self.perm_mD = perm_mD
-        self.perm_m2 = self.perm_mD * 9.8692326671601e-13 * 1e-3  # reservoir permeability in m2
+        self.perm_m2 = self.perm_mD * 9.8692326671601e-13 * 1e-3  # target layer permeability in m2
         # Input parameters fluid properties
         self.rho_fluid = rho_fluid
         self.Cp_fluid = Cp_fluid
         # Input parameters pT
-        self.T_ref = T_surf
+        self.T_surf = T_surf
         self.T_grad = Temp_grad
-        self.p_ref = 101325 # pressure at the surface of the earth in Pascal
+        self.p_surf = 101325 # pressure at the surface of the earth in Pascal
         self.p_grad = 9792100 # pressure gradient with depth, in Pa/m
         # Input parameters wells
         self.q_m3_h = flow_m3_h
@@ -66,23 +68,25 @@ class Doublet:
         self.w_space = w_space
         self.w_diam = 0.2032
         self.T_inj = T_inj
-        self.p_prod = self.p_ref + ((self.r_d + self.r_h / 2) * 1e-3 * self.p_grad)  # undisturbed p at reservoir depth
-        self.T_prod = self.T_ref + ((self.r_d + self.r_h / 2) * 1e-3 * self.T_grad)  # production temperature degC
+        self.p_prod = self.p_surf + ((self.r_d + self.r_h / 2) * 1e-3 * self.p_grad)  # undisturbed p at target depth
+        self.T_prod = self.T_surf + ((self.r_d + self.r_h / 2) * 1e-3 * self.T_grad)  # production temperature in °C
         self.mu_0 = PropsSI('viscosity', 'T', self.T_prod + 273.15, 'P', self.p_prod, 'Water')
         self.mu_inj = PropsSI('viscosity', 'T', self.T_inj + 273.15, 'P', self.p_prod, 'Water')
         # Input parameters economic
         self.pump_eta = pump_effi / 100
 
 
-    # This part describes the physics that happen in a geothermal doublet. Everything here are definitions.
-    # That means, here we tell the computer HOW to calculate something, for example how to calculate lifetime.
-    # But it does not calculate it yet, it just knows how to do it.
-    # The calculation happens when we call the function in the main file.
+    # This part describes the physics that happen in a geothermal doublet.
+    # The following blocks of code, that all start with the word "def", are called FUNCTIONS.
+    # A function tells the computer HOW to calculate something, for example how to calculate lifetime.
+    # But it does not do the calculation yet.
+    # For that, we have to CALL the function. Like in code cell 4 in the notebook.
 
     def lmbda(self):
         """
+        This is a helper function. You are not using it directly, but some of the other functions use it.
         This function calculates how much faster the cold water moves than the cold temperature.
-        This depends on the physical properties of the water and the rock.
+        That depends on the physical properties of the water and the rock.
         """
         mobility_lambda = self.poro * self.rho_fluid * self.Cp_fluid / (
                 (1 - self.poro) * self.rho_rock * self.Cp_rock + self.rho_fluid * self.Cp_fluid * self.poro)
@@ -90,8 +94,9 @@ class Doublet:
 
     def mu(self, r):
         """
-        This function calculates the viscosity of the water (how "honey-like" it is). Viscosity changes
-        with temperature, which is why it is different between the two wells.
+        This is a helper function. You are not using it directly, but some of the other functions use it.
+        This function calculates the viscosity of the water (how "honey-like" it is). Viscosity is different between
+        the two wells, because it depends on temperature.
         """
         if r < 0:
             mu = self.mu_inj
@@ -104,8 +109,10 @@ class Doublet:
 
     def dp_wells(self):
         """
-        This function calculates the pressure difference between the two wells. It depends on many factors. For example,
-        when you want a high flowrate of water, you have to pump harder.
+        This is a helper function. You are not using it directly, but some of the other functions use it.
+        This function calculates the pressure difference between the two wells. That depends on many factors. For example,
+        when you want a high flowrate, you will have a very low pressure at the production well and a very high pressure
+        at the injection well. The difference between them will be big.
         """
         c_inj = self.mu_inj / self.perm_m2 * self.q_m3_s / (2 * np.pi * self.r_h)
         c_prd = self.mu_0 / self.perm_m2 * self.q_m3_s / (2 * np.pi * self.r_h)
@@ -116,8 +123,8 @@ class Doublet:
 
     def t_breakthrough(self):
         """
-        This function calculates the moment when colder water starts to arrive at the production well. It depends on
-        several factors. For example, when the wells are further apart, it takes longer.
+        This function calculates the time when colder water from the injection well starts to arrive at the production well.
+        That depends on several factors. For example, when the wells are further apart, it takes longer.
         """
         self.t_cold_yrs = (self.poro / self.lmbda() *
                            (2 * np.pi * self.r_h) / self.q_m3_h * self.w_space ** 2 / 6) / (365 * 24)
